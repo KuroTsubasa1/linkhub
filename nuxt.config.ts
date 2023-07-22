29a0eeac4551225c9bfd11bc5bb27af3eae35265
Lasse Harm <lasse@lasseharm.space>
2023-07-22 23:47:20 +0200

    Added primevue component lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
+  css: [
+    'primevue/resources/themes/saga-blue/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css',
+    'primeflex/primeflex.css'
+  ],
+  build: {
+    transpile: ['primevue']
+  },
 })
